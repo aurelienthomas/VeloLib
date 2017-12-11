488a1f28d13820cfb52b213e115d458de09087fe
aurelien.thomas <aurelien.thomas@etu.univ-tlse2.fr>
2017-12-11 17:57:33 +0100

    LuL fautque ça devienne un prjet MAVEN

--- src/Init.java
+++ src/Init.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Init {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList <Station>listeStation = new ArrayList<>();
 
 	}
+
+	GsonBuilder gsonb = new GsonBuilder();
+	        gsonb.registerTypeAdapter(Station.class, new StationDeserializer());
+	        Gson gson = gsonb.create();
 
 }

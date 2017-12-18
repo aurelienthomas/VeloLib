c1af96623c4ab7a499e71cb4f332949ee79734fd
jonathan.fernando-johnson <jonathan.fernando-johnson@mathinfo.i-univ-tlse2.fr>
2017-12-18 09:49:41 +0100

    ajout cle google map

--- Kuma/parseJSON.py
+++ Kuma/parseJSON.py
@@ -1,7 +1,15 @@
-import json, requests
+import json
+import requests
 
 from pprint import pprint
 
+#parse données statiques
 with open('../Toulouse.json') as data_file:    
     data = json.load(data_file)
 pprint(data)
+
+#parse API
+url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=adcf8bb9b76004df8324b59f7dc0a0a74e1658f0'
+
+resp = requests.get(url=url)
+data = json.loads(resp.text)

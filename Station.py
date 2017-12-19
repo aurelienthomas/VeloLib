4f647a4686d743a666539466e1836087d5a41c88
aurelien.thomas <aurelien.thomas@etu.univ-tlse2.fr>
2017-12-19 08:48:37 +0100

    Keskece

--- Station.py
+++ Station.py
@@ -1,8 +1,9 @@
 import json
 import math
 
+
 class Station():
-    def __init__(self, numero,longitude,name,adress,latitude):
+    def __init__(self, numero, longitude, name, adress, latitude):
         self.numero = numero
         self.longitude = longitude
         self.latitude = latitude
@@ -11,7 +12,7 @@ class Station():
 
 
 def __init__(self, j):
-         self.__dict__ = json.loads(j)
+    self.__dict__ = json.loads(j)
 
 
 def chargerJson(fichier):
@@ -20,20 +21,18 @@ def chargerJson(fichier):
         json_dict = json.load(f)
     return json_dict
 
+
 def graph(liste_donnees):
-    #import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
     axe_x = []
     axe_y = []
     for donnee in liste_donnees:
         axe_x.append(donnee["longitude"])
         axe_y.append(donnee["latitude"])
-    plt.plot(axe_x,axe_y)
+    plt.plot(axe_x, axe_y)
     plt.xlabel('longitude')
     plt.ylabel('latitude')
     plt.show()
-
-
-
 
 
 liste_station = chargerJson("Toulouse.json")
@@ -42,8 +41,11 @@ distance_min = 3600000
 for station in liste_station:
     for station_proche in liste_station:
         if station['name'] != station_proche["name"]:
-            distance = math.sqrt((station["longitude"]-station_proche["longitude"]) * (station["longitude"]-station_proche["longitude"]) + (station["latitude"] - station_proche["latitude"]) * (station["latitude"] - station_proche["latitude"]))
-            if distance < distance_min:
-                station_la_plus_proche = station_proche
+            if station['name'] != station_proche["name"]:
+                distance = math.sqrt((station["longitude"] - station_proche["longitude"]) * (
+                    station["longitude"] - station_proche["longitude"])
+                                     + (station["latitude"] - station_proche["latitude"]) * (
+                                         station["latitude"] - station_proche["latitude"]))
+                if distance < distance_min:
+                    station_la_plus_proche = station_proche
     station["Station_proche"] = station_la_plus_proche
-print (liste_station)

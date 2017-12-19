585f74541671c2d44e1438ca3997f40e4914d10d
aurelien.thomas <aurelien.thomas@etu.univ-tlse2.fr>
2017-12-19 10:38:37 +0100

    mod git ignore et gmap

--- Gmaps.py
+++ Gmaps.py
@@ -1,7 +1,6 @@
-import googlemaps
-from datetime import datetime
 import json
-from pprint import pprint
+
+import googlemaps
 import requests
 
 """gmaps = googlemaps.Client(key='Add Your Key here')
@@ -19,6 +18,8 @@ directions_result = gmaps.directions("Sydney Town Hall",
                                      mode="transit",
                                      departure_time=now)
 """
+
+
 # parse API
 def station_a_recharger():
     url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=adcf8bb9b76004df8324b59f7dc0a0a74e1658f0'
@@ -29,12 +30,17 @@ def station_a_recharger():
     station_a_recharger = []
 
     for data in liste_data:
-        if data["available_bike_stands"] < data["bike_stands"]/2 and data["status"] == "OPEN":
+        if data["available_bike_stands"] < data["bike_stands"] / 2 and data["status"] == "OPEN":
             station_a_recharger.append(data)
 
     return station_a_recharger
 
+
 def gmaps():
     station = station_a_recharger()
     gmaps = googlemaps.Client(key='AIzaSyAqq2AUug4t3ovjFt_AP4KiCqNhVdIuedM')
-    reverse_geocode_result = gmaps.reverse_geocode((station[0].position.lat, station[0].position.lng))
+    print(station[0])
+    reverse_geocode_result = gmaps.reverse_geocode((station[0]["position"]["lat"], station[0]["position"]["lng"]))
+
+
+gmaps()

14974d97ae3c8c5f6accf52442f12cf3b040e8d6
aurelien.thomas <aurelien.thomas@etu.univ-tlse2.fr>
2017-12-19 08:59:53 +0100

    ParseJson

---
+++ parseJSON.py
@@ -0,0 +1,31 @@
+import json
+from pprint import pprint
+
+import requests
+
+
+# parse données statiques
+def open_fichier_Json(fichier):
+    with open(fichier) as data_file:
+        data = json.load(data_file)
+
+
+# pprint(data)
+
+# parse API
+def station_a_recharger():
+    url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=adcf8bb9b76004df8324b59f7dc0a0a74e1658f0'
+
+    resp = requests.get(url=url)
+    liste_data = json.loads(resp.text)
+
+    station_a_recharger = []
+
+    for data in liste_data:
+        if data["available_bike_stands"] != data["bike_stands"] and data["status"] == "OPEN":
+            station_a_recharger.append(data)
+
+    pprint(station_a_recharger)
+
+
+station_a_recharger()
